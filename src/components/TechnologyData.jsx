66b4f43e2969a53ce70ba41e24fc990a416973c9
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-02-17 15:35:55 -0300

    -- technology finalizada --

--- src/components/TechnologyData.jsx
+++ src/components/TechnologyData.jsx
@@ -1,7 +1,10 @@
 import { spacelaunch } from "../data/data";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const TechnologyData = ({ tecnologiaInfo }) => {
+  const location = useLocation();
+  const params = useParams();
+
   return (
     <>
       <h2 className="planetas-h2">
@@ -10,11 +13,27 @@ const TechnologyData = ({ tecnologiaInfo }) => {
       <img src={tecnologiaInfo.img} className="tecnologia-img" alt="" />
       <div className="tecnologia-centrar">
         {spacelaunch.map((tecnologia) => (
-          <Link to={`/technology/${tecnologia.id}`} state={tecnologia}>
+          <Link
+            className={`tecnologia-navegacion ${
+              location.pathname === "/technology"
+                ? "tecnologia-navegacion-activa-child"
+                : ""
+            } ${
+              Number(params.id) === tecnologia.id
+                ? "tecnologia-navegacion-activa"
+                : ""
+            }`}
+            key={tecnologia.id}
+            to={`/technology/${tecnologia.id}`}
+            state={tecnologia}
+          >
             {tecnologia.id}
           </Link>
         ))}
       </div>
+      <h2 className="technology-h2">{"The terminology".toUpperCase()}</h2>
+      <h1 className="technology-h1">{tecnologiaInfo.titulo.toUpperCase()}</h1>
+      <p className="technology-p">{tecnologiaInfo.descripcion}</p>
     </>
   );
 };
