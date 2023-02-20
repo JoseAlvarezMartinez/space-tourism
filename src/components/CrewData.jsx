69bbc06d0f7f72414b932842a4523fcf5e215c0d
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-02-20 13:44:32 -0300

    -- tablet responsive crew --

--- src/components/CrewData.jsx
+++ src/components/CrewData.jsx
@@ -1,5 +1,7 @@
 import { Link, useLocation, useParams,useLoaderData } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 const CrewData = ({ tripulante }) => {
+  const tablet = useMediaQuery({ query: "(min-width: 650px)" });
   const data = useLoaderData()
   const location = useLocation();
   const params = useParams();
@@ -8,7 +10,7 @@ const CrewData = ({ tripulante }) => {
       <h2 className="planetas-h2">
         02 <span>{"Meet your crew".toUpperCase()}</span>
       </h2>
-      <div className="contenedor-img">
+      {!tablet && <>  <div className="contenedor-img">
         <img
           src={tripulante.img}
           className={`${
@@ -17,7 +19,35 @@ const CrewData = ({ tripulante }) => {
           alt=""
         />
       </div>
+      
       <div className="contenedor-navegacion-tripulantes">
+      {data.map((tripulante) => (
+        <Link
+          key={tripulante.id}
+          to={`/crew/${tripulante.id}`}
+          className={` 
+          ${
+            location.pathname == "/crew" &&
+            tripulante.nombre == "Douglas Hurley"
+              ? "circulo-activo"
+              : ""
+          }
+            ${
+              tripulante.id === Number(params.id)
+                ? "circulo-activo "
+                : "circulo-navegacion"
+            } `}
+          state={tripulante}
+        ></Link>
+      ))}
+    </div>
+      </>
+      }
+
+      <h3 className="crew-h3">{tripulante.cargo.toUpperCase()}</h3>
+      <h2 className="crew-h2">{tripulante.nombre.toUpperCase()}</h2>
+      <p className="crew-p">{tripulante.descripcion}</p>
+      {tablet &&  <div className="contenedor-navegacion-tripulantes">
         {data.map((tripulante) => (
           <Link
             key={tripulante.id}
@@ -37,10 +67,18 @@ const CrewData = ({ tripulante }) => {
             state={tripulante}
           ></Link>
         ))}
+      </div>}
+      {tablet &&  <div className="contenedor-img">
+        <img
+          src={tripulante.img}
+          className={`${
+            tripulante.nombre.includes("Douglas") ? "douglas" : "tripulacion"
+          }`}
+          alt=""
+        />
+        
       </div>
-      <h3 className="crew-h3">{tripulante.cargo.toUpperCase()}</h3>
-      <h2 className="crew-h2">{tripulante.nombre.toUpperCase()}</h2>
-      <p className="crew-p">{tripulante.descripcion}</p>
+      }
     </>
   );
 };
